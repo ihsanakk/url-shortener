d8c6e43667ac57e9a0969868a4d4fe39ea7aaf4c
ihsanakk <aakkoyunlu.ihsan@gmail.com>
2022-07-14 11:08:35 +0300

    exception handler response

--- src/main/java/com/tempter/urlshortener/exception/ControllerExceptionHandler.java
+++ src/main/java/com/tempter/urlshortener/exception/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.tempter.urlshortener.exception;
 
 import com.tempter.urlshortener.exception.exceptions.URLShortenerException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler({URLShortenerException.class})
-    public String handleURLExceptions(Exception e) {
+    public ResponseEntity<String> handleURLExceptions(Exception e) {
         log.error(e.getMessage());
-        return e.getMessage();
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 
 }
